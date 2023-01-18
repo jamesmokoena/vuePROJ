ee29f21a68d72cb50ab48cb536e3829a7aae4df0
Ntethe James Mokoena <mokoenantethe@gmail.com>
2023-01-18 13:17:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <img class="logo" src="./assets/logo-1932539__340.webp"  >
+  
+  <signup/>
+</template>
+
+<script>
+import signup from './components/signup.vue'
+export default {
+  name: 'App',
+  components:{
+    signup 
+  }
+ 
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+
+<style> 
+.logo{
+    width: 150px;
+    padding-bottom: 30px;
+    
+}
+
+</style>
