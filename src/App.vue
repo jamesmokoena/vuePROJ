40709ee20147ad264e0197442d92e2aa46145575
Ntethe James Mokoena <mokoenantethe@gmail.com>
2023-01-19 10:54:55 +0200

    i am in a process of building an api for login

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,35 @@
 <template>
   <img class="logo" src="./assets/logo-1932539__340.webp"  >
-  
   <signup/>
+
+  <div class="signup">
+    <div class="main">  	
+        <input type="checkbox" id="chk" aria-hidden="true">
+
+            <div class="signup">
+                <form>
+                    <input type="text" v-model="name" name="txt" placeholder="Name" required="">
+                    <input type="email" v-model="email" name="email" placeholder="Email" required="">
+                    <input type="password" v-model="password" name="pswd" placeholder="Password" required="">
+                    <button> Sign up</button>
+                </form>
+            </div>
+
+            <div class="login">
+                <form>
+                    <label for="chk"  aria-hidden="true">Login</label>
+                    <input type="email"  v-model="email" name="email" placeholder="Email" required="">
+                    <input type="password"  v-model="password" name="pswd" placeholder="Password" required="">
+                    <button v-on:click="signup"> >Login</button>
+                </form>
+            </div>
+    </div>
+</div>
+
+
+
+
+  
 </template>
 
 <script>
@@ -9,7 +37,19 @@ import signup from './components/signup.vue'
 export default {
   name: 'App',
   components:{
-    signup 
+    signup,
+    data(){
+      return {
+        name: "",
+        email: "",
+        password: ""
+      }},
+      
+    methods: {
+        signup()
+         {
+            console.warn("signup",this.name,this.email.this.password)
+         }} 
   }
  
 }
--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,29 +1,5 @@
 <template>
 
-    <div class="signup">
-        <div class="main">  	
-            <input type="checkbox" id="chk" aria-hidden="true">
-    
-                <div class="signup">
-                    <form>
-                        <input type="text" name="txt" placeholder="User name" required="">
-                        <input type="email" name="email" placeholder="Email" required="">
-                        <input type="password" name="pswd" placeholder="Password" required="">
-                        <button>Sign up</button>
-                    </form>
-                </div>
-    
-                <div class="login">
-                    <form>
-                        <label for="chk" aria-hidden="true">Login</label>
-                        <input type="email" name="email" placeholder="Email" required="">
-                        <input type="password" name="pswd" placeholder="Password" required="">
-                        <button>Login</button>
-                    </form>
-                </div>
-        </div>
-    </div>
-
    
    
 </template>
